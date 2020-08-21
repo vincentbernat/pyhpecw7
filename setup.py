cc66ba99e453ef08d5470effa120dad728d46989
Vincent Bernat <vincent@bernat.ch>
2020-08-21 20:08:35 +0200

    setup: modernize a bit packaging
    And release a new version

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from distutils.core import setup
-from setuptools import find_packages
+import setuptools
 
-setup(
-  name='py3hpecw7',
-  packages=find_packages(),
-  version='0.1.0',
-  description='Python package to simplify working with HPE Comware7 based devices',
-  author='HPE',
-  license='Apache2',
-  url='https://github.com/vincentbernat/pyhpecw7',
-  classifiers=[
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='py3hpecw7',
+    packages=setuptools.find_packages(),
+    version='0.1.1',
+    description='Python package to interact with HPE Comware7 based devices',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='HPE',
+    license='Apache2',
+    url='https://github.com/vincentbernat/pyhpecw7',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
@@ -19,15 +23,15 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-  keywords='HPE Comware7 FlexFabric Netconf API ',
+    keywords='HPE Comware7 FlexFabric Netconf API ',
 
   package_data={'pyhpecw7': ['utils/templates/textfsm_temps/*.tmpl']},
-  install_requires=[
-      'textfsm>=0.2.1',
-      'lxml',
-      'ncclient',
-      'scp',
-      'ipaddr>=2.1.11',
-      'paramiko'
-  ],
+    install_requires=[
+        'textfsm>=0.2.1',
+        'lxml',
+        'ncclient',
+        'scp',
+        'ipaddr>=2.1.11',
+        'paramiko'
+    ],
 )
